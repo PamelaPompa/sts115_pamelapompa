09a17f250456ed027168bec387dea6eb1b9cdcbe
Pamela_Pompa <pgpompa@ucdavis.edu>
2025-03-12 20:22:40 -0700

    Uploading HW 8

---
+++ submitted_assignments_pamelapompa/hw08_completed.r
@@ -0,0 +1,304 @@
+# Directions:
+
+# This file contains homework questions for the lecture on data visualization.
+# Questions appear as comments in the file. 
+
+# Please see the Grading Criteria Canvas Page for specific guidance on what
+# we expect from you regarding assignment responses.
+
+# Once you have completed the assignment, follow the Submission Instructions 
+# on Canvas Pages section to add, commit, and push this to your GitHub repository. 
+
+# Some questions have multiple parts - make sure to read carefully and
+# answer all of them. The majority of points lost in homework come from
+# careless skipping over question parts.  
+
+###############################################################################
+
+
+# Write a R code snippet that uses regular expressions to find all occurrences of 
+# dates in the string variable text that are in the format "dd/mm/yyyy" and replace 
+# them with the format "yyyy-mm-dd". The text variable contains multiple dates in 
+# the "dd/mm/yyyy" format interspersed with other text. For example, if text is 
+# "The event was held on 23/04/2021 and the next event will be on 05/10/2022.", 
+# the output should be "The event was held on 2021-04-23 and the next 
+# event will be on 2022-10-05.".
+#
+# Ensure your code dynamically handles the text variable, meaning it should work 
+# for any string input following the mentioned pattern. Use relevant functions from 
+# the stringr package, any other package, or base R for your solution. 
+# [Code completion + comprehension]
+
+  library("tidyverse") #provides me with ggplot2 and stringr
+
+  wrong_date_format <- "The event was held on 23/04/2021 and the next event will be on 05/10/2022."
+  #im using this text variable as a test to see if my code works
+  
+  #im using str_replace_all to find all occurrences of strings written in the wrong date format,
+  #and to replace them with the right date format
+  fixed_date_format <- str_replace_all( wrong_date_format,
+    "(\\d{2})/(\\d{2})/(\\d{4})", #pattern to match "dd/mm/yyyy"
+    "\\3-\\2-\\1"                    #rearranging into "yyyy-mm-dd"
+  )
+  
+  # The \\d{2} refers to extracting two numbers (expressed as text) that fit this format (the days),
+  # then we extract the next two numbers (the months), and finally, we extract the next four numbers (the year).
+  # The "\\d" specifically tells the program to look for any digit (0–9). The {2} part means we want exactly two digits,
+  # which corresponds to the day and month in the "dd/mm/yyyy" format, while {4} means we want four digits for the year.
+  # Without \\d, the program wouldn't know to specifically match digits. It might instead match other characters or fail
+  # to identify the correct portions of the text.
+  
+  # The extraction sequences are followed by "/" to mimic how the date is originally formatted (d/m/y), and
+  # the new format expresses the extracted sequences with hyphens to match the desired format. Also,
+  # the sequences are expressed as 3, 2, 1 to indicate that we want the year first, the month next, and the day last.
+  
+  print(fixed_date_format) #checking to see if it works
+
+
+# Write an R code snippet that assigns the string "I am here.  Am I alive" to
+# a variable "x" and uses an Escape Sequence to put a newline between the two
+# sentences. [Code completion + comprehension]
+
+  x <- "I am here. \nAm I alive"
+  cat(x)
+  message(x)
+
+  #'I assigned the given sentence to the x variable, and the way I utilized a Escape Sequence is by
+  #'writing a "\" followed by a "n" which stands for the new line action. The "\" in a string is meant to escape
+  #'the normal behavior of a string, signaling that the character following it is a special command rather than
+  #'part of the text itself. After this special command is done, you return back to the text. 
+  #'Afterwards, to check that the Escape Sequence works as expected, I have two commands, cat(x) and message(x)
+  #'in order to double check that the new line shows up.
+  #'since cat(x) will concatenate everything to work as expected and message(x) prints out the message, also
+  #'registering the \n Escape Sequence written
+
+
+# Write an R code snippet that assigns the following string to the variable "y:"
+#
+# She said, "Hi!"
+# [Code completion + comprehension]
+
+  y <- 'She said, "Hi!"'
+  
+  cat(y)
+  message(y)
+
+  
+  #'I wasn't sure if the question wanted the variable name to be y, or "y:", but the cat command would not work when
+  #'I tried to do cat("y:") or cat(y:), so I figured the semicolon in the question was meant to be outside of the 
+  #'quotation marks, so I had the variable name simply be y
+  #'Anyways, in order to differentiate the quotation marks that are part of the text, I had the string be marked
+  #'with the single quotation symbol (') instead of the double ("). That way, when the text includes the Double
+  #'Quotation marks, it knows not to end the actual text yet, since that would need a single quotation symbol to
+  #'end it. I used the same functions as the prior question to see if the message prints as expected, and it does
+
+
+
+# [TEXT ANSWER] Explain what a Text Encoding is:
+# [Comprehension]
+
+  #'Text encoding refers to how a computer stores text; we need this because a computer can't actually
+  #'#read text like how humans do. Anyways, an encoding is a system for mapping characters to numbers. 
+  #'#There are various types such as ASCII, UTF-8 or UTF-16. In general, a character
+  #'will map to a specific number. For example, in ASCII, the character 'a' maps to the number 97. (This info
+  #'is from the course reader)
+
+
+# Write an R code snippet that creates a vector of all containing the words in the string, 
+# "He wanted to say hello but was afraid".  Then use the stringr library to locate any 
+# occurrences of the string "hello" in your vector.
+# [Code completion + interpretation]
+
+  string <- "He wanted to say hello but was afraid."
+  #This is the string containing the given text from the prompt
+  
+  string_vector <- str_split(string, " ")[[1]]
+  #this function will split the string by the spaces in the sentence, and it provides a vector with every word
+  #as an element
+  #the [[1]] is needed because str_split() always returns a list, even when splitting a single string,
+  #in order to handle cases where multiple strings may be input. Since the result for each input is stored as
+  #a separate list element, we use [[1]] to extract the first (and only) element when dealing with a single string
+  
+  str_detect(string_vector, 'hello') #returns T/F for every element, returns TRUE if the 
+                                     #string you want is found, in this case the 5th element is TRUE
+  str_view(string_vector, 'hello') #tells you what index 'hello' is at (in index 5)
+  #either of these could work
+  
+
+# Section "15.7 Corpus Analytics" of the reader contains a tutorial on performing
+# corpus analytics on a Document Term Matrix of 19th Century novels.  A Document Term
+# Matrix is a matrix that contains information about the number of times that a word 
+# appears in each text in a corpus.  In class, we calculated the word frequencies for
+# the novel _Wuthering Heights_.  There "data" folder in the Files area of Canvas for 
+# this course contains a file names dtm.rds which holds this type of count information
+# for every normalized text in the collection of novels we worked with in class. 
+# 
+#     First, download that file to your course working directory.   
+
+#     Then, recreate a working version of the code in section 15.7 of the reader below.
+#     Note that before you can use the code in that section you will need to read 
+#     dtm.rds file into the dtm variable so that the data in the DTM is available to
+#     the rest of the code.
+# [Code completion + interpretation]
+  
+    #OVERALL GOAL/ACHIEVEMENT: I fixed the tm_map() function in order to have a working version
+    #of the code from the 15.7 section of the reader
+    
+    library("tm") #I need for tm_map function and the DocumentTermMatrix function
+
+    dtm <- readRDS("../data/dtm.rds")
+    
+    dtm$ncol #this is to get the number of columns in the dtm, which basically gives us information on the word size
+    #there are 34925 words
+    dtm$nrow #number of rows, aka the number of documents/texts that make up the matrix
+    #there are 18 texts
+    
+    dtm$dimnames$Docs
+    #the document names are just labeled by numbers instead of by the text title, so to fix that we need to
+    #use our manifest to provide the corresponding title to every row
+    
+    manifest <- read.csv("../data/C19_novels_manifest.csv", row.names = 1)
+    manifest
+    
+    dtm$dimnames$Docs <- manifest$title #here we assign the right names to the texts
+    dtm$dimnames$Docs #check to see that it works
+    
+    inspect(dtm) #this gives us a high level view of the corpus
+    #shows us an overview of the corpus; has some of the texts alongside some of the words and their frequency
+    
+    findFreqTerms(dtm, 1000) #provides us with the terms that appear more than 1000 times in the corpus
+    
+    findAssocs(dtm, "boat", .85) #This provides us with words that are used in connection with the word
+                                 #'boat' and have at least a .85 correlation to it
+                                 
+    writing <- findAssocs(dtm, "writing", .85)
+    writing[[1]][1:15] #same goal as above, but this only provides us with the first 15 with the strongest correlation
+    
+    
+    term_counts <- as.matrix(dtm) #transforms the dtm into a matrix
+    #each row represents a document, and each column represents a unique word
+    #The cells in the matrix store word counts for each word-document pair
+    
+    term_counts <- data.frame(sort(colSums(term_counts), decreasing = TRUE))
+    #colSums(term_counts) finds the sum of counts for each word across all documents (in the corpus), which
+    #gives you the overall frequency of every word in the corpus
+    #sort(..., decreasing = TRUE) organizes the words in descending order of their total counts,
+    #so the most frequently occurring words appear first
+    term_counts <- cbind(newColName = rownames(term_counts), term_counts)
+    colnames(term_counts) <- c("term", "count")
+    #using the cbind function adds a column of the term names (aka the words themselves)
+    #and the column names are relabeled to make it clearer what they represent (the word and its respective count)
+    
+    term_counts #to check that it worked as expected
+    
+    ggplot(term_counts[1:50, ]) +
+      aes(x = fct_reorder(term, -count), y = count) +
+      geom_bar(stat = "identity") +
+      theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+      ) +
+      labs(
+        title = "Top 50 words in 18 Nineteenth-Century Novels",
+        x = "Word",
+        y = "Count"
+      )
+    #this bar graph will show the top 50 words (so the top 50 from term_counts)
+    #The issue is that most of these words aren't special to our 19th century collection, they're just very
+    #common English words in general, so we need to be more specific to find out what makes our corpus special
+    
+    
+    cleaned_corpus <- readRDS("../data/C19_novels_cleaned.rds")
+    #Im using this data because that's the data used in the course reader
+    cleaned_corpus <- VCorpus(VectorSource(cleaned_corpus)) 
+    #convert to VCorpus; I had problems later on in my script and I think it was because this corpus was
+    #not in the right format, so it wasn't properly working with the DocumentTermMatrix function from the 
+    #tm library. I asked Microsoft Copilot how to put it in the right format for it to work with this function
+    #and it gave me this line. This type of corpus will allow the TF-IDF calculation to be computed on it
+    
+    cleaned_corpus <- tm_map(
+      cleaned_corpus,
+      content_transformer(function(x) str_remove_all(x, "\\'s"))
+    )
+    #Im cleaning the corpus from contractions as suggested by the course reader before I proceed with the
+    #creation of the matrix
+    
+    #This is the course reader's tm_map() function that didn't work
+    #cleaned_corpus <- tm_map(
+    #  cleaned_corpus, str_remove_all, pattern = "\\'s", replacement = " "
+    #)
+    
+    #What I did was just utilize the str_remove_all function the right way by plugging in what I wanted ro remove
+    #I asked Microsoft Copilot for help on how to apply the function to a corpus though and it told me that I
+    #should utilize "content_transformer"; this function wraps regular string-processing functions (like str_replace_all)
+    #so they focus only on the text content of each document while ignoring metadata.
+    
+    dtm_tfidf <- DocumentTermMatrix(
+      cleaned_corpus,
+      control = list(weighting = weightTfIdf)
+    )
+    #this incorporates the TF-IDF calculation to push down the scores of common words while boosting
+    #the score for the rarer words
+    
+    manifest <- read.csv("../data/C19_novels_manifest.csv", row.names = 1)
+    #I'm reinitializing manifest to double check there's no errors
+    dtm_tfidf$dimnames$Docs <- manifest$title
+
+    
+    #### this bottom section for the new graph works the same as above but with TD-IDF weighted added
+    
+    tfidf_counts <- as.matrix(dtm_tfidf)
+    tfidf_counts <- data.frame(sort(colSums(tfidf_counts), decreasing = TRUE))
+    tfidf_counts <- cbind(newColName = rownames(tfidf_counts), tfidf_counts)
+    colnames(tfidf_counts) <- c("term", "tfidf")
+    
+    tfidf_counts #checking to see that it works
+    
+    ggplot(data = tfidf_counts[1:50, ]) +
+      aes(x = fct_reorder(term, -tfidf), y = tfidf) +
+      geom_bar(stat = "identity") +
+      theme(
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+      ) +
+      labs(
+        title = "Words with the 50-highest TF-IDF scores in 18 Nineteenth-Century Novels",
+        x = "Word",
+        y = "TF-IDF"
+      )
+    
+    #We got a lot of names because the names are unique to each text
+    
+    findAssocs(dtm_tfidf, terms = "boat", corlimit = .85)
+    findAssocs(dtm_tfidf, terms = "writing", corlimit = .85)
+    #the correlations match the expected ones from the course reader
+    #the correlated words are more specific to the context rather than just being common terms now
+    
+    
+    #this next section focuses on the uniqueness of the novels now
+    tfidf_df <- as.matrix(dtm_tfidf)
+    tfidf_df <- as.data.frame(t(tfidf_df))
+    colnames(tfidf_df) <- manifest$title
+    #the 't' in the second line is for transpose; it flips the rows and columns of the matrix
+    #this will help us focus on how each novel relates to the terms it uses
+    
+      
+    ordering <- order(tfidf_df$Dracula, decreasing = TRUE)
+    rownames(tfidf_df[ordering[1:50], ])
+    #the first line organizes our rows by the highest value for a given column; helps us find the unique terms
+    #in every novel
+    #this example is for Dracula
+    #there are no contractions
+    
+    ordering <- order(tfidf_df$Frankenstein, decreasing = TRUE)
+    rownames(tfidf_df[ordering[1:50], ])
+    
+    ordering <- order(tfidf_df$SenseandSensibility, decreasing = TRUE)
+    rownames(tfidf_df[ordering[1:50], ])
+    
+    # no contractions for these two either
+
+
+
+
+
+
+
